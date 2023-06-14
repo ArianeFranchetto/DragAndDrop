d2bf65fbbfc44725f61acc050d538c9db18eb232
Ariane de Almeida Franchetto <nanifranchetto@gmail.com>
2023-06-13 22:31:40 -0300

    lógica
    lógica finalizada

--- drag-and-drop/src/App.vue
+++ drag-and-drop/src/App.vue
@@ -12,8 +12,28 @@ export default {
 const getList = (list) => {
   return items.value.filter((item) => item.list == list)
 }
+
+const startDrag = (event, item) => {
+  console.log(item)
+  event.dataTransfer.dropEffect = 'move'
+  event.dataTransfer.effectAllowed = 'move'
+  event.dataTransfer.setData('itemID', item.id)
+
+}
+
+const onDrop = (event, list) => {
+  const itemID = event.dataTransfer.getData('itemID')
+  const item = items.value.find((item) => item.id == itemID)
+  item.list =  list
+ 
+  
+}
+
 return {
   getList,
+  onDrop,
+  startDrag,
+
 }
 
   }
@@ -22,12 +42,37 @@ return {
 </script>
 
 <template>
-  <div class="drop-zone">
-    <div v-for="item in getList(1)" :key="item.id" class="drag-el">
+<div class="drop-zone"
+@drop="onDrop($event, 1)"
+@dragenter.prevent
+@dragover.prevent
+>
+<div 
+    v-for="item in getList(1)" 
+    :key="item.id" 
+    class="drag-el"
+    draggable="true"
+    @dragstart="startDrag($event, item)"
+    >
     {{ item.title }}
+
     </div>
-    <div v-for="item in getList(2)" :key="item.id" class="drag-el">
+</div>
+
+<div class="drop-zone"
+@drop="onDrop($event, 2)"
+@dragenter.prevent
+@dragover.prevent
+>
+    <div 
+    v-for="item in getList(2)" 
+    :key="item.id" 
+    class="drag-el"
+    draggable="true"
+    @dragstart="startDrag($event, item)"
+    >
     {{ item.title }}
+
     </div>
     </div>
 </template>
